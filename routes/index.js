52fecc9e4c6ad865a6de5378557d7330776d20ed
hotdog16 <hotdoggg136@gmail.com>
2023-04-20 12:31:39 +0900

    project 생성 및 간단한 초기세팅(라우터)

--- routes/index.js
+++ routes/index.js
@@ -2,8 +2,34 @@ const express = require('express');
 const router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/', function (req, res, next) {
+    res.render('index', {title: 'Express'});
 });
-
+router.post('/', function (req, res, next) {
+    res.render('index', {title: 'Express'});
+});
+router.get('/login', function (req, res, next) {
+    res.render('index', {title: 'Express'});
+});
+router.post('/login', function (req, res, next) {
+    res.render('index', {title: 'Express'});
+});
+router.get('/join', function (req, res, next) {
+    res.render('index', {title: 'Express'});
+});
+router.post('/join', function (req, res, next) {
+    res.render('index', {title: 'Express'});
+});
+router.get('/board', function (req, res, next) {
+    res.render('index', {title: 'Express'});
+});
+router.post('/board', function (req, res, next) {
+    res.render('index', {title: 'Express'});
+});
+router.get('/', function (req, res, next) {
+    res.render('index', {title: 'Express'});
+});
+router.get('/', function (req, res, next) {
+    res.render('index', {title: 'Express'});
+});
 module.exports = router;
