b07016bca7fcd6b663377fd0b3d8156ab130338f
권미애 <alxo9974@naver.com>
2023-04-21 14:33:04 +0900

    회원가입 구현중

--- routes/index.js
+++ routes/index.js
@@ -1,43 +1,42 @@
 const express = require("express");
 const router = express.Router();
-const {createProduct, addProduct} = require('../controllers/product');
+const { createProduct, addProduct } = require("../controllers/product");
+const { addUser } = require("../controllers/user");
 /* GET home page. */
 
 router.get("/main", function (req, res, next) {
-    res.render("index");
+  res.render("index");
 });
-router.get('/', function (req, res, next) {
-    res.render('index');
-
+router.get("/", function (req, res, next) {
+  res.render("index");
 });
 router.post("/", function (req, res, next) {
-    res.render("index", {title: "Express"});
+  res.render("index", { title: "Express" });
 });
 router.get("/login", function (req, res, next) {
-    res.render("login");
+  res.render("login");
 });
 router.post("/login", function (req, res, next) {
-    res.render("index", {title: "Express"});
+  res.render("index", { title: "Express" });
 });
 router.get("/join", function (req, res, next) {
-    res.render("join", {title: "Express"});
+  res.render("join", { title: "Express" });
 });
-router.post("/join", function (req, res, next) {
-    res.render("index", {title: "Express"});
-});
+router.post("/join", addUser);
+
 router.get("/board", function (req, res, next) {
-    res.render("index", {title: "Express"});
+  res.render("index", { title: "Express" });
 });
 router.post("/board", function (req, res, next) {
-    res.render("index", {title: "Express"});
+  res.render("index", { title: "Express" });
 });
 
 router.get("/", function (req, res, next) {
-    res.render("index", {title: "Express"});
+  res.render("index", { title: "Express" });
 });
 router.get("/", function (req, res, next) {
-    res.render("index", {title: "Express"});
+  res.render("index", { title: "Express" });
 });
-router.get('/product', createProduct);
-router.post('/product', addProduct);
+router.get("/product", createProduct);
+router.post("/product", addProduct);
 module.exports = router;
