acb69f3bca1d36913af54c58ee76404e9a1d6658
kyounghee <zamong79@naver.com>
2023-04-25 11:47:52 +0900

    230425-miae데이터와 비교후 수정

--- app.js
+++ app.js
@@ -9,21 +9,39 @@ const dotenv = require("dotenv");
 const passport = require("passport");
 const fs = require('fs');
 const multer = require('multer');
-const {sequelize} = require("./models");
-const passportConfig = require("./passport");
 dotenv.config();
 
-const indexRouter = require("./routes/index");
-const usersRouter = require("./routes/users");
 const noticeRouter = require("./routes/notice");
 const productRouter = require("./routes/product");
 const orderRouter = require("./routes/order");
+const indexRouter = require("./routes/index");
+const usersRouter = require("./routes/users");
 const boardRouter = require("./routes/board");
+const {sequelize} = require("./models");
 
 const app = express();
 
-// 파일 업로드를 위해 디렉토리가 있는지 확인하고 없다면 생성
+// view engin setup
+app.set("views", path.join(__dirname, "views"));
 
+const passportConfig = require("./passport");
+
+passportConfig(); // 패스포트 설정
+
+app.set("view engine", "html");
+nunjucks.configure("views", {
+    express: app,
+    watch: true,
+});
+
+// 파일 업로드를 위해 디렉토리가 있는지 확인하고 없다면 생성
+app.listen(3000, ()=>{
+    const dir = './public/images';
+    if(!fs.existsSync(dir)){
+        fs.mkdirSync(dir);
+    }
+    console.log('서버실행');
+})
 app.use(morgan("dev"));
 app.use(express.static(path.join(__dirname, "public")));
 app.use("/img", express.static(path.join(__dirname, "uploads")));
@@ -41,16 +59,8 @@ app.use(
         },
     })
 );
-
-passportConfig(); // 패스포트 설정
-
-// view engine setup
-app.set("views", path.join(__dirname, "views"));
-app.set("view engine", "html");
-nunjucks.configure("views", {
-    express: app,
-    watch: true,
-});
+app.use(passport.initialize());
+app.use(passport.session());
 
 app.use("/", indexRouter);
 app.use("/users", usersRouter);
@@ -58,13 +68,23 @@ app.use("/notice", noticeRouter);
 app.use("/product", productRouter);
 app.use("/order", orderRouter);
 app.use("/board", boardRouter);
-
-app.use(passport.initialize());
-app.use(passport.session());
-
-
+// catch 404 and forward to error handler
+app.use(function (req, res, next) {
+    next(createError(404));
+});
+// app.use(function (req, res, next) {
+//   next(createError(404));
+// });
 
 // error handler
+app.use(function (err, req, res, next) {
+    // set locals, only providing error in development
+    res.locals.message = err.message;
+    res.locals.error = req.app.get("env") === "development" ? err : {};
+})
+// render the error page
+
+
 app.use((req, res, next) => {
     const error = new Error(`${req.method} ${req.url} 라우터가 없습니다.`);
     error.status = 404;
@@ -77,5 +97,10 @@ app.use((err, req, res, next) => {
     res.status(err.status || 500);
     res.render("error");
 });
-
 module.exports = app;
+
+
+
+
+
+
--- controllers/mypage.js
+++ controllers/mypage.js
@@ -1,3 +1,5 @@
+const { users } = require("../models");
+
 exports.mypage = async (req, res, next) => {
   console.log("req:body ======>1111", req.user);
   const user = req.user;
@@ -11,6 +13,29 @@ exports.mypage = async (req, res, next) => {
 exports.mypageUpdate = async (req, res) => {
   const user = req.user;
   console.log("mypageuser -------------------->", user);
+  res.render("mypage_update", { user });
+};
 
-  res.render("mypage_update");
+exports.mypageUpdateAdd = async (req, res, next) => {
+  console.log(req.body);
+  try {
+    await users.update(
+        {
+          u_pwd: req.body.u_pwd,
+
+          s_no: req.body.s_no,
+          p_img: req.body.p_img,
+        },
+        {
+          where: {
+            p_no: req.body.p_no,
+          },
+        }
+    );
+    res.redirect("/product");
+  } catch (e) {
+    console.error(e);
+    next(e);
+  }
+  res.send("수정이 완료되었습니다");
 };
