6d5f63f7de1fb859ddb285b60b6dfe76cb38353f
hotdog16 <hotdoggg136@gmail.com>
2023-04-26 11:12:14 +0900

    상품 리스트 페이징 처리 구현 중

--- controllers/product.js
+++ controllers/product.js
@@ -39,35 +39,99 @@ exports.addProduct = async (req, res, next) => {
 }
 
 exports.listProduct = async (req, res) => {
-    // console.log('controllers/listProduct ======>');
-    // ///////////////////////////////////////////////////
-    // // let {searchType, keyword} = req.query;
-    // const contentSize = Number(process.env.CONTENTSIZE);
-    // console.log('contentSize : ', contentSize);
-    // const currentPage = Number(req.query.currentPage) || 1;
-    // console.log('currentPage : ', currentPage);
-    // const {limit, offset} = getPagination(currentPage, contentSize);
-    // // keyword = keyword ? keyword : "";
-    // let dataAll = await products.findAll({limit, offset})
-    // let dataCountAll = await products.findAndCountAll({limit, offset})
-    // const pagingData = getPagingData(dataCountAll, currentPage, limit);
-    // let list = dataAll;
-    ///////////////////////////////////////////////////
+    // console.log("REST API Get Method - Read All");
+    // // 페이지 크기
+    // var countPerPage = req.query.countperpage;
+    // // 페이지 번호
+    // var pageNo = req.query.pageno;
+    // // 페이지 사이즈
+    // var pageSize = req.query.pagesize;
+    //
+    // if (countPerPage == undefined || typeof countPerPage == "undefined" || countPerPage == null) {
+    //     countPerPage = 10;
+    // } else {
+    //     countPerPage = parseInt(countPerPage);
+    // }
+    // if (pageSize == undefined || typeof pageSize == "undefined" || pageSize == null) {
+    //     pageSize = 10;
+    // } else {
+    //     pageSize = parseInt(pageSize);
+    // }
+    // if (pageNo == undefined || typeof pageNo == "undefined" || pageNo == null) {
+    //     pageNo = 0;
+    // } else {
+    //     pageNo = parseInt(pageNo);
+    // }
+    //
+    // if (pageNo > 0) {
+    //     // 전체 크기
+    //     var totalCount = boardList.length;
+    //     // 마지막 페이지 번호(전체 페이지 크기)
+    //     var lastPageNo = Math.floor(totalCount / countPerPage) + (totalCount % countPerPage== 0 ? 0 : 1);
+    //     // 시작 페이지 번호
+    //     var startPageNo = 1;
+    //     // 페이지 사이즈로 페이지 번호를 나눈 몫만큼 페이지 시작 번호 변경
+    //     var start = Math.floor(pageNo / pageSize);
+    //     if (start >= 1) {
+    //         // 그렇지만 나머지가 없으면 현재 페이지 번호가 마지막 페이지 번호와 같아 감소
+    //         if (pageNo % pageSize == 0){
+    //             start--;
+    //         }
+    //         startPageNo = (start * pageSize) + 1;
+    //     }
+    //     // 종료 페이지 번호
+    //     var endPageNo = (startPageNo - 1) + pageSize;
+    //     // 그렇지만 종료 페이지 번호가 마지막 페이지 번호보다 크면 마지막 페이지 번호로 변경
+    //     if (endPageNo > lastPageNo) {
+    //         endPageNo = lastPageNo;
+    //     }
+    //     // 이전 페이지 사이즈 번호
+    //     var prevPageSizeNo = startPageNo - 1;
+    //     // 이전 페이지 사이즈 번호 활성화 여부
+    //     var enablePrevPageSizeNO = true;
+    //     if (prevPageSizeNo == 0) {
+    //         enablePrevPageSizeNO = false;
+    //     }
+    //     // 다음 페이지 사이즈 번호
+    //     var nextPageSizeNo = endPageNo + 1;
+    //     // 다음 페이지 사이즈 번호 활성화 여부
+    //     var enableNextPageSizeNO = true;
+    //     if (nextPageSizeNo > lastPageNo) {
+    //         enableNextPageSizeNO = false;
+    //     }
+    //     // 시작 번호
+    //     var startItemNo = ((pageNo - 1) * countPerPage);
+    //     // 종료 번호
+    //     var endItemNo = (pageNo * countPerPage) - 1;
+    //     // 종료 번호가 전체 크기보다 크면 전체 크기로 변경
+    //     if (endItemNo > (totalCount - 1)) {
+    //         endItemNo = totalCount - 1;
+    //     }
+    //     var boardPageList = [];
+    //     if (startItemNo < totalCount) {
+    //         for (var index = startItemNo; index <= endItemNo; index++) {
+    //             boardPageList.push(boardList[index]);
+    //         }
+    //     }
+    //     // 페이지네이션 정보
+    //     var paginationInfo = {};
+    //     paginationInfo.totalCount = totalCount;
+    //     paginationInfo.countPerPage = countPerPage;
+    //     paginationInfo.pageSize = pageSize;
+    //     paginationInfo.startPageNo = startPageNo;
+    //     paginationInfo.endPageNo = endPageNo;
+    //     paginationInfo.lastPageNo = lastPageNo;
+    //     paginationInfo.pageNo = pageNo;
+    //     paginationInfo.prevPageSizeNo = prevPageSizeNo;
+    //     paginationInfo.enablePrevPageSizeNO = enablePrevPageSizeNO;
+    //     paginationInfo.nextPageSizeNo = nextPageSizeNo;
+    //     paginationInfo.enableNextPageSizeNO = enableNextPageSizeNO;
+    //     res.json({success:true, data:boardPageList, pagination:paginationInfo});
+    // } else {
+    //     res.json({success:true, data:boardList});
+    // }
     let limit = 10;
     let offset = 0 + Number((req.body.page? req.body.page : 1) - 1) * limit;
-    // console.log('offset : ', offset);
-    // let totalCnt = products.findAndCountAll({
-    //     limit : limit,
-    //     offset : offset,
-    //     order:[['p_no', 'desc']],
-    //     include: {
-    //         model: stores,
-    //         as: "s_no_store",
-    //         required: true
-    //     }
-    // });
-    // console.log('totalCnt : ', totalCnt);
-    // totalCnt = totalCnt/10;
     products.findAndCountAll({
         limit: 10,
         offset : offset,
@@ -79,11 +143,7 @@ exports.listProduct = async (req, res) => {
         }
     })
         .then((productList) => {
-            // res.status(200).json({products:productList});
-            // res.render('product/list',{products:productList});
-            // res.status(200).json({products:productList});
             res.render('product/list',{products:productList});
-            // res.render('product/list',{products:productList, list, pagingData});
         })
         .catch((err) => {
             res.send(err);
