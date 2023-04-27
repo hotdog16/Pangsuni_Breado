9ad8d595c6617da7aaad0cd4ccd8bd6c175f0831
hotdog16 <hotdoggg136@gmail.com>
2023-04-27 19:46:39 +0900

    제품 예약 기능 구현

--- controllers/order.js
+++ controllers/order.js
@@ -1,7 +1,27 @@
 const { stores, users, orders } = require("../models");
+const {Sequelize} = require("sequelize");
 
 exports.addOrder  = async (req, res) => {
-    console.log('req.body :', req.body);
-    console.log('req.user :', req.user);
-    res.json({body:req.body, user:req.user});
+    try{
+        const loopCnt = req.body.p_no.length;
+        const {p_no, o_cnt, o_pickup_dt} = req.body;
+        const today = new Date();
+        const pickupDay = new Date(today.setDate(ttt3.getDate() + Number(o_pickup_dt)));
+        const ttt5 = new Date(ttt3.setDate(ttt3.getDate() - 1));
+        for(let i=0; i<loopCnt; i++){
+            if(Number(o_cnt[i]) !== 0){
+                await orders.create({
+                    o_no: null,
+                    u_id : req.user.u_id,
+                    p_no : p_no[i],
+                    o_reg_dt : Sequelize.Sequelize.literal('now()'),
+                    o_pickup_dt : pickupDay,
+                    o_cnt : Number(o_cnt[i])
+                })
+            }
+        }
+        res.redirect('/store');
+    }catch (e) {
+        console.error(e);
+    }
 };
