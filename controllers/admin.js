d7586896cffda5a10417234bba0b7ec3626a2b67
hotdog16 <hotdoggg136@gmail.com>
2023-05-02 21:32:58 +0900

    1. 게시판 경로 및 파일명 수정 2. 관리자 페이지 기능 구현 중

--- controllers/admin.js
+++ controllers/admin.js
@@ -1,8 +1,34 @@
 const { Sequelize } = require("sequelize");
-const { users } = require("../models");
+const { users,orders,products } = require("../models");
 
-exports.adminOrder = (req, res) => {
-  res.render("admin/order", { title: "주문관리" });
+exports.adminOrder = async (req, res) => {
+  let limit = 10; // sql select 쿼리문의 order by limit 부분
+  let offset = 0 + Number((req.query.page ? req.query.page : 1) - 1) * limit; // sql select 쿼리문의 order by offset 부분
+  let checkNum = (req.query.page ? req.query.page : 1); // 페이지 네비게이션 부분에 페이징을 위한 변수 초기화
+  checkNum = Math.floor(checkNum / 10) * 10; // 10자리에서 내림을 해서 10개씩 끊어주려고 위해 재할당
+  const listOrder = await orders.findAndCountAll({
+    limit,
+    offset,
+    order:[['o_no', 'desc']],
+    include:{
+      model:products,
+      as:'p_no_product',
+      required: true
+    }
+  });
+  let navCheck = Math.ceil(listOrder.count / 10) * 10; // 페이지 네비게이션을 체크하기 위한 변수로 초기화
+  navCheck = navCheck / 10; // 초기화 후 쉽게 체크하기 위해 재할당
+  const num = []; // 페이지 네비게이션에 나올 숫자들을 담을 배열을 선언
+  for (let i = checkNum; i < checkNum + 10; i++) { // checkNum 변수를 이용해서 10개씩 담기 위한 반복문 사용
+    if (i < navCheck) {
+      num.push(i + 1);
+    }
+  }
+  if (Number.isNaN(req.query.page) || req.query.page > navCheck) {
+    res.status(400).json('숫자만 눌러주세요! 현재 페이지는 없습니다!');
+  }
+  console.log('orders : ',listOrder);
+  res.render("admin/order", {title: "주문관리", orders: listOrder, currentPage: offset, num, checkNum, user: req.user});
 };
 
 exports.adminProduct = (req, res) => {
