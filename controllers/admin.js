fdccbee12046008dba609028da68229d88b4336e
권미애 <alxo9974@naver.com>
2023-05-03 17:57:15 +0900

    관리자페이지연결, 관리자-회원관리 수정중

--- controllers/admin.js
+++ controllers/admin.js
@@ -1,5 +1,5 @@
 const { Sequelize } = require("sequelize");
-const { users } = require("../models");
+const {stores, users, orders, products, regions} = require("../models");
 
 exports.adminOrder = (req, res) => {
   res.render("admin/order", { title: "주문관리" });
@@ -10,7 +10,8 @@ exports.adminProduct = (req, res) => {
 };
 
 exports.adminMember = async (req, res, next) => {
-  const { u_no, u_id, u_name, u_tel, u_email, u_grade } = req.body;
+  const u_no = req.params.u_no;
+  console.log('req.body ====> admin', req.user)
   try {
     const user_list = await users.findAll();
     // console.log("유저목록 -----------");
@@ -18,6 +19,7 @@ exports.adminMember = async (req, res, next) => {
     for (u in user_list) {
       console.log(u);
     }
+
     // console.log("유저목록 끝 ============================");
     res.render("admin/member", { list: user_list });
   } catch (error) {
@@ -33,3 +35,47 @@ exports.adminBoard = (req, res) => {
 exports.adminStore = (req, res) => {
   res.render("admin/store", { title: "스토어관리" });
 };
+
+
+exports.member = (req,res) =>{
+  const user = req.user;
+  console.log("req.user ===> admin=======>",req.user)
+}
+
+exports.DetailMember = async(req,res) =>{
+
+  console.log("userdetail111=======>",req.params);
+
+  const {id} = req.params;
+  const user = req.user;
+
+  const userdetail = await users.findOne({
+    raw: true,
+    where: {
+      u_no : id
+    }
+  })
+  console.log("userdetail=======>",userdetail);
+  if(userdetail){
+    res.status(200).json({userdetail});
+  }
+}
+
+exports.DetailOrderMember = async(req,res) =>{
+
+  console.log("userdetail111=======>",req.params);
+
+  const {id} = req.params;
+  const user = req.user;
+
+  const userdetailorder = await orders.findOne({
+    raw: true,
+    where: {
+      u_id : id
+    }
+  })
+  console.log("userdetail=======>",userdetailorder);
+  if(userdetailorder){
+    res.status(200).json({userdetailorder});
+  }
+}
--- routes/admin.js
+++ routes/admin.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const router = express.Router();
-const { adminOrder, adminProduct, adminMember, adminBoard, adminStore } = require("../controllers/admin");
+const { DetailOrderMember,DetailMember, adminOrder, adminProduct, adminMember, adminBoard, adminStore, member } = require("../controllers/admin");
 
 router.use((req, res, next) => {
   res.locals.user = req.user;
@@ -8,9 +8,16 @@ router.use((req, res, next) => {
 });
 
 router.get("/order", adminOrder);
+
 router.get("/product", adminProduct);
+
 router.get("/member", adminMember);
+router.get("/memberDetail/:id", DetailMember);
+router.get("/memberDetailOrder/:id", DetailOrderMember);
+router.post("/member", member)
+
 router.get("/board", adminBoard);
+
 router.get("/store", adminStore);
 
 module.exports = router;
