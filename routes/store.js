231784f5b46c54d34535f8def4be7228c2304c17
hotdog16 <hotdoggg136@gmail.com>
2023-05-03 18:35:50 +0900

    가게 추가 기능 연결 중

--- routes/store.js
+++ routes/store.js
@@ -10,6 +10,7 @@ router.get("/", isLoggedIn, selectListRegions);
 // router.post("/:r_no", isLoggedIn, selectListRegionStores);
 router.post("/", isLoggedIn, selectListRegionStores);
 router.get("/add", isLoggedIn, createStore);
+router.post("/add", isLoggedIn, createStore);
 
 router.get("/detail/:no", isLoggedIn, detailStore);
 router.post("/detail/:no", isLoggedIn, detailStore);
