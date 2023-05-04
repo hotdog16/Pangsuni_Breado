90f14a26431b5a1bc94c5133128c7e6a82a409f9
권미애 <alxo9974@naver.com>
2023-05-04 10:16:51 +0900

    관리자-회원관리 수정중

--- routes/admin.js
+++ routes/admin.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const router = express.Router();
-const { DetailOrderMember,DetailMember,adminOrder, adminProduct, adminMember, adminBoard, adminStore,adminBoard2,deleteBoard, member } = require("../controllers/admin");
+const { DetailMember,adminOrder, adminProduct, adminMember, adminBoard, adminStore,adminBoard2,deleteBoard, member } = require("../controllers/admin");
 
 router.use((req, res, next) => {
   res.locals.user = req.user;
@@ -14,7 +14,7 @@ router.get("/product", adminProduct);
 router.get("/member", adminMember);
 
 router.get("/memberDetail/:id", DetailMember);
-router.get("/memberDetailOrder/:id", DetailOrderMember);
+// router.get("/memberDetailOrder/:id", DetailOrderMember);
 router.post("/member", member)
 
 router.get("/board", adminBoard); // 화면이동
