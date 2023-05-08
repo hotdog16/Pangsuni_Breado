e2f1cdd59971e24044f28bc3f24821ae03a3c94b
권미애 <alxo9974@naver.com>
2023-05-08 17:51:40 +0900

    main css수정

--- controllers/page.js
+++ controllers/page.js
@@ -1,3 +1,6 @@
+const { Sequelize } = require("sequelize");
+const {stores, users, orders, products, regions} = require("../models");
+
 exports.renderLogin = (req, res) => {
   res.render("login", { title: "로그인" });
 };
@@ -22,3 +25,23 @@ exports.renderMain = async (req, res, next) => {
   });
   // next();
 };
+
+exports.main = async(req,res,next) => {
+  console.log(req.body)
+  try{
+    console.log('지역번호 : ', req.body);
+    // const r_no = req.params.r_no;
+    const s_no = req.body.s_no;
+    const storemain = await stores.findAll({
+      order:Sequelize.literal('rand()'), limit:8,
+      paranoid: true,
+      required: false,
+      nest:true
+    })
+    console.log('controllers : ',storemain);
+    return res.status(200).json({storemain:storemain, s_no})
+}catch (e) {
+    console.error(e);
+    return res.status(500).json({store:e});
+}
+}
