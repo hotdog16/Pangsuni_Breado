53c895250b5e4f760a764e8a24f61257b5559a57
kyounghee <zamong79@naver.com>
2023-05-08 17:17:21 +0900

    230508(2)-board-수정 및 삭제 구현

--- routes/board.js
+++ routes/board.js
@@ -1,7 +1,19 @@
 const express = require("express");
 const router = express.Router();
 const {isLoggedIn} = require('../middlewares/index')
-const {selectListBoard, addBoard, formBoard, selectOneBoard, CommentView, CommentWrite, CommentAdd, deleteBoard, test2} = require("../controllers/board");
+const {
+    selectListBoard,
+    addBoard,
+    formBoard,
+    selectOneBoard,
+    CommentView,
+    CommentWrite,
+    CommentAdd,
+    deleteBoard,
+    test2,
+    modifyFormBoard,
+    modifyBoard
+} = require("../controllers/board");
 // 여기는 주소가 http://localhost:3000/board/ get방식 qnaList
 // 여기는 주소가 http://localhost:3000/board/ poet방식 addBoard
 router.get("/list/:bt_no", selectListBoard); // 게시판 리스트로이동
@@ -15,8 +27,8 @@ router.get("/view/:no",selectOneBoard);
 router.get("/delete/:b_no", deleteBoard);
 router.post("/delete/:b_no", deleteBoard);
 
-// router.get("/modify/:b_no", ModifyQnaList);
-// router.post("/modify/:b_no", ModifyQnaList);
+router.get("/modify/:b_no", modifyFormBoard);
+router.post("/modify", modifyBoard);
 
 // 댓글 기능은 따로 router와 컨트롤러 만들어야 함
 router.get("/commentsview/:no", CommentView);
