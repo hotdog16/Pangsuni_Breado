ad86d59dd752da55fc004cf7d6a1f529319332a3
kyounghee <zamong79@naver.com>
2023-05-09 15:03:02 +0900

    230509(3)-admin-게시판-수정하기 완료

--- controllers/admin/adminBoard.js
+++ controllers/admin/adminBoard.js
@@ -1,4 +1,5 @@
-const {regions,users,products,stores} = require("../../models");
+const {regions,users,products,stores,board} = require("../../models");
+const {Sequelize} = require("sequelize");
 
 
 exports.addFormBoard = async (req, res) => {
@@ -6,7 +7,32 @@ exports.addFormBoard = async (req, res) => {
 };
 
 exports.adminModifyBoard = async (req,res)=>{
-    const b_no = req.params.p_no;
-    const board = await products.findOne({where:{b_no}});
-    res.render('admin/board/adminModifyBoard', {board, page});
+    const b_no = req.params.b_no;
+    const boards = await board.findOne({where:{b_no}});
+    res.render('admin/board/adminModifyBoard', {board:boards});
+}
+
+exports.adminModifyBoardPost = async(req, res) => {
+    const {b_no, b_title, b_content}=req.body;
+    console.log(b_no);
+    console.log(b_title);
+    console.log(b_content);
+    try{
+        const boards =await board.update({
+            b_title,
+            b_content,
+            b_mod_dt:Sequelize.Sequelize.literal('now()')
+        },{
+            where:{
+                b_no
+            }
+        });
+        if(boards === null){
+            return res.status(400).json({msg:'updateError'});
+        }
+        return res.status(200).json({msg:'success'});
+    }catch (err) {
+        console.error(err);
+        return res.status(500).json({msg:err});
+    }
 }
--- routes/board.js
+++ routes/board.js
@@ -13,7 +13,6 @@ const {
     test2,
     modifyFormBoard,
     modifyBoard,
-    adminModifyBoard
 } = require("../controllers/board");
 const {modifyFormProduct, modifyProduct} = require("../controllers/admin/adminProduct");
 const {upload} = require("../middlewares/uploads");
@@ -42,7 +41,5 @@ router.post("/commentsview/:no", CommentAdd);
 // router.post("/listpaging", qnaListPaging);
 // router.get("/test", test); // 게시판 리스트로이동
 router.get("/test2/:bt_no", test2); // 게시판 리스트로이동
-
-// router.get('/board/:b_no', adminModifyBoard);
 
 module.exports = router;
