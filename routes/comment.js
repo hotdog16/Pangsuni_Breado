a1f62748716deb186af310d5349987787e88453d
hotdog16 <hotdoggg136@gmail.com>
2023-05-10 10:18:10 +0900

    사용하지 않는 코드 삭제 및 정렬 중

--- routes/comment.js
+++ routes/comment.js
@@ -5,6 +5,8 @@ const router = express.Router();
 
 router.get("/selectList/:b_no", selectListComment); // 게시판 리스트로이동
 router.get("/selectOne/:c_no", selectOneComment); // 게시판 리스트로이동
+
 router.post('/add2', addComment);
 router.post('/modify', modifyComment);
+
 module.exports = router;
--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,4 @@
 const express = require("express");
-const passport = require("passport")
-
 const { join, login, logout, idCheck, emailCheck, telCheck } = require("../controllers/auth");
 const { renderJoin, renderMain, renderLogin, main } = require("../controllers/page");
 
@@ -19,8 +17,6 @@ router.post("/",main);
 router.get("/login",isNotLoggedIn, renderLogin);
 router.post("/login",isNotLoggedIn, login);
 
-// router.post("/loginCheck", loginCheck);
-
 router.get("/join", isNotLoggedIn, renderJoin);
 router.post("/join", isNotLoggedIn, join);
 
@@ -31,17 +27,5 @@ router.post("/emailCheck", emailCheck);
 router.post("/telCheck", telCheck);
 
 router.get("/logout", isLoggedIn, logout);
-// router.post("/logout", isLoggedIn, logout);
-
-
-// 지도 테스트 중
-router.get("/map", (req, res) => {
-  const mapAPI = process.env.KAKAO_MAP;
-  res.render("kakaoTest", { mapAPI });
-});
-router.get("/map2", (req, res) => {
-  const mapAPI = process.env.KAKAO_MAP;
-  res.render("kakaoTest2", { mapAPI });
-});
 
 module.exports = router;
