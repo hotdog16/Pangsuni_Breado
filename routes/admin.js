4799f48e4e3c6de51817819c3c0aa6a71d76b84e
kyounghee <zamong79@naver.com>
2023-05-10 09:37:33 +0900

    230510(1)-게시판-이름 변경 및 경로 수정

--- routes/admin.js
+++ routes/admin.js
@@ -5,15 +5,9 @@ const {
   deleteMember,
   DetailMember,
   adminMember,
-  adminBoard,
-  adminBoard2,
-  deleteBoard,
-  member,
-  deleteComment
+  member
 } = require("../controllers/admin");
-const {addFormBoard,adminModifyBoard, adminModifyBoardPost} = require('../controllers/admin/adminBoard');
 const {isLoggedIn, whoisAdmin} = require("../middlewares");
-
 
 router.use((req, res, next) => {
   res.locals.user = req.user;
@@ -32,23 +26,6 @@ router.get("/member/detail/:u_no", memberdetail);
 router.post("/member/detail/:u_no", DetailMember);
 // router.get("/memberDetailOrder/:id", DetailOrderMember);
 router.post("/member/delete", deleteMember)
-
-// board
-router.get("/board", adminBoard); // 화면이동
-
-router.get("/board2", adminBoard2); // axios를 이용한 비동기 (이름 바꿔야함)
-
-router.get("/board/add", isLoggedIn, whoisAdmin, addFormBoard);
-
-router.post("/board/delete", deleteBoard);
-
-router.get("/board/modify/:b_no", adminModifyBoard);
-router.post("/board/modify", adminModifyBoardPost);
-
-
-router.post('/comment/delete', deleteComment);
-
-
 
 // router.get("/store", adminStore);
 // router.get('/store/selectList', selectListStore);
